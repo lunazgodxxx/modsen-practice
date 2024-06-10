0611a5687e3329ee33ecc81096865ca6c56b2ffb
Yahor Maksimchik <moseyarthas@gmail.com>
2024-06-10 18:23:04 +0300

    impl: multiplication-table.js

---
+++ codewars/multiplication-table.js
@@ -0,0 +1,21 @@
+multiplicationTable = function(size) {
+  const array = new Array(size);
+  const initialValue = 0
+  for (let i = 0; i < size; i++) {
+    array[i] = new Array(size).fill(initialValue);
+  }
+
+  counter = 0
+  for (let x = 0; x < array.length; x++) {
+    for (let y = 0; y < array[x].length; y++) {
+      array[x][y] = (x + 1) * (y + 1)
+      // console.log(`[${x}][${y}] - ${array[x][y]}`);
+    }
+  }
+
+  return array
+}
+
+
+
+console.log(multiplicationTable(3));
