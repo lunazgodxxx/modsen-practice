b574f0d863853614d9fa6aedaa49b0f238a17ec2
Yahor Maksimchik <moseyarthas@gmail.com>
2024-06-13 01:30:56 +0300

    feat: prisma db connection, models impl

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [UserModule]
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.dev',
+    }),
+    UserModule,
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  const globalPrefix = configService.get<string>('GLOBAL_PREFIX');
+
+  app.setGlobalPrefix(globalPrefix);
+  await app.listen(port, () => {
+    Logger.debug(`Listen at http://localhost:${port}/${globalPrefix}`);
+  });
 }
 bootstrap();
